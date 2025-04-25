27c8dbb67c56e48f57479be20539dd80dfa46eae
NTPU <NTPU@503-11>
2025-04-25 16:31:35 +0800

    0425_1631

---
+++ src/main/java/dao/BaseDao.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+// 用來讓不同的 xxxDao 透過 extends BaseDao 可以連接 MySQL
+public class BaseDao {
+  protected static Connection conn;
+	
+  static {
+    // 連線參數
+    String username = "root";
+    String password = "p@ssw0rd";
+    String dbUrl = "jdbc:mysql://localhost:3306/todolist?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true&useSSL=false";
+
+
+    // 建立連線
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver"); // 註冊 MySQL Driver //連線到 Tomcat伺服器會需要用到
+      conn = DriverManager.getConnection(dbUrl, username, password); // 連線建立
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
---
+++ src/main/java/dao/TodoListDaoImpl.java
@@ -0,0 +1,124 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+import model.entity.Todo;
+
+public class TodoListDaoImpl extends BaseDao implements TodoListDao{
+
+	@Override
+	public List<Todo> findAllTodos(){
+		List<Todo> todos = new ArrayList<>();
+		
+		String sql = "select id, text, completed from todo order by id";
+		try(Statement state = conn.createStatement();
+			ResultSet rs = state.executeQuery(sql)){
+			//逐一尋訪 rs 中的每一筆紀錄，並將每一筆紀錄轉乘 Todo 物件，最後加入倒 todos 集合中
+			while(rs.next()) {
+				Todo todo = new Todo();
+				todo.setId(rs.getInt("id"));
+				todo.setText(rs.getString("text"));
+				todo.setCompleted(rs.getBoolean("completed"));
+				
+				todos.add(todo);
+			}
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return todos;
+	}
+
+	@Override
+	public Todo getTodo(Integer id) {
+		String sql = "select id, text, completed from todo where id=?";
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, id);
+			
+			try(ResultSet rs = pstmt.executeQuery()){
+				if(rs.next()) {
+					Todo todo = new Todo();
+					todo.setId(rs.getInt("id"));
+					todo.setText(rs.getString("text"));
+					todo.setCompleted(rs.getBoolean("completed"));
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void addTodo(Todo todo) {
+		String sql = "insert into todo(text, completed) value(?, ?)";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, todo.getText());
+			pstmt.setBoolean(2, todo.getCompleted());
+			
+			int rowcount = pstmt.executeUpdate();//得到異動筆數
+			System.out.println("新增 todo 筆數: " + rowcount);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updateTodoComplete(Integer id, Boolean completed) {
+		String sql = "update todo set completed=? where id=?";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+			
+			pstmt.setBoolean(1, completed);
+			pstmt.setInt(2, id);
+			
+			int rowcount = pstmt.executeUpdate();
+			System.out.println("修改 todo 筆數: " + rowcount);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void updateTodoText(Integer id, String text) {
+		String sql = "update todo set text=? where id=?";
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+			
+			pstmt.setString(1, text);
+			pstmt.setInt(2, id);
+			
+			int rowcount = pstmt.executeUpdate();
+			System.out.println("修改 todo 筆數: " + rowcount);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void deleteTodo(Integer id) {
+		String sql = "delete from todo where id=?"; 
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){					
+			pstmt.setInt(1, id);
+					
+			int rowcount = pstmt.executeUpdate();
+			System.out.println("修改 todo 筆數: " + rowcount);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+}
